01c7e57b51cd08c713593a0bc878ff5876eecb8a
734692305 <liuhuijun_web@sina.com>
2020-06-09 01:46:46 +0800

    initial commit

---
+++ generators/app/index.js
@@ -0,0 +1,53 @@
+const Generator = require('yeoman-generator')
+module.exports = class extends Generator {
+  prompting() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname // appname 为项目生成目录名称
+      }
+    ])
+    .then(answers => {
+      // answers => { name: 'user input value' }
+      this.answers = answers
+    })
+  }
+
+  writing () {
+    // 把每一个文件都通过模板转换到目标路径
+
+    const templates = [
+      'package.json',
+      'postcss.config.js',
+      'README.md',
+      'public/favicon.ico',
+      'public/index.html',
+      'src/App.vue',
+      'src/main.js',
+      'src/router.js',
+      'src/assets/logo.png',
+      'src/components/HelloWorld.vue',
+      'src/store/actions.js',
+      'src/store/getters.js',
+      'src/store/index.js',
+      'src/store/mutations.js',
+      'src/store/state.js',
+      'src/utils/request.js',
+      'src/views/About.vue',
+      'src/views/Home.vue'
+    ]
+
+    templates.forEach(item => {
+      // item => 每个文件路径
+      this.fs.copyTpl(
+        this.templatePath(item),
+        this.destinationPath(item),
+        this.answers
+      )
+    })
+  }
+}
+
+
